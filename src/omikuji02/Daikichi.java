5bb8be70385727f5aad18df295a1d6fa9cadea34
tatiana <tatiana@192.168.150.123>
2021-09-02 16:21:32 +0900

    コミット

--- src/omikuji02/Daikichi.java
+++ src/omikuji02/Daikichi.java
@@ -1,5 +1,9 @@
 package omikuji02;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Daikichi extends  Omikuji{
 
     @Override
@@ -9,6 +13,20 @@ public class Daikichi extends  Omikuji{
 
     @Override
     public void setUnsei() {
+        // ArrayListの生成
+        List <String> list = new ArrayList<>();
+
+        // 要素の追加
+        list.add("大吉");
+        list.add("中吉");
+        list.add("小吉");
+        list.add("末吉");
+        list.add("吉");
+        list.add("凶");
+
+        int index = new Random().nextInt(list.size());
+        String result = list.get(index);
+        System.out.println(result);
 
     }
 
--- src/omikuji02/Main.java
+++ src/omikuji02/Main.java
@@ -3,11 +3,13 @@ package omikuji02;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
+
+//        Daikichi daikichi = new Daikichi();
+//        daikichi.setUnsei();
 
       //ファイル読み込みで使用する３つのクラス
         FileInputStream fi = null;
@@ -22,20 +24,22 @@ public class Main {
           is = new InputStreamReader(fi);
           br = new BufferedReader(is);
 
-          String line;
-          ArrayList csvData = new ArrayList();
-          while ( ( line = br.readLine()) != null ) {
-            String[] cols = line.split(",");
-            csvData.add(cols);
+       // readLineで一行ずつ読み込む
+          String line; // 読み込み行
+          String[] data; // 分割後のデータを保持する配列
+          while ((line = br.readLine()) != null) {
+          // lineをカンマで分割し、配列dataに設定
+          data = line.split(",");
+          if (data.length > 3) {
+              // 読み込んだCSVファイルの内容を出力
+            System.out.println("運勢:"  + data[0]);
+            System.out.println("願い事:"  + data[1]);
+            System.out.println("商い:"  + data[2]);
+            System.out.println("学問:"  + data[3]);
+          }
+          System.out.println();
           }
 
-          // 読み込みデータの表示
-          for ( String[] row : csvData ) {
-            System.out.println("願い事　: " + row[0]);
-            System.out.println("商い　: " + row[1]);
-            System.out.println("学問　: " + row[2]);
-            System.out.println();
-          }
 
 //          String line;
 //          int index = 0;
@@ -44,53 +48,13 @@ public class Main {
 //                  String[] data = line.split(",");
 //                  if (data.length > 3) {
 //                      // 読み込んだCSVファイルの内容を出力
+//                      System.out.println("運勢:"  + data[0]);
 //                      System.out.println("願い事:"  + data[1]);
 //                      System.out.println("商い:"  + data[2]);
 //                      System.out.println("学問:"  + data[3]);
 //                  }
 //              }
-//              index++;
 //          }
-
-//          //読み込み行
-//          String line;
-//
-//          //読み込み行数の管理
-//          int i = 0;
-//
-//          //列名を管理する為の配列
-//          String[] arr = null;
-//
-//          //1行ずつ読み込みを行う
-//          while ((line = br.readLine()) != null) {
-//
-//            //先頭行は列名
-//            if (i == 0) {
-//
-//              //カンマで分割した内容を配列に格納する
-//              // arr = { "no","name","age","gender","bloodtype" };
-//              arr = line.split(",");
-//
-//            } else {
-//
-//              //カンマで分割した内容を配列に格納する
-//              String[] data = line.split(",");
-//
-//              //配列の中身を順位表示する。列数(=列名を格納した配列の要素数)分繰り返す
-//              int colno = 0;
-//              for (String column : arr) {
-//                System.out.println(column + ":" + data[colno]);
-//                colno++;
-//
-//              }
-//
-//            }
-//
-//            //行数のインクリメント
-//            i++;
-//
-//          }
-
         } catch (Exception e) {
           e.printStackTrace();
         } finally {
@@ -99,6 +63,7 @@ public class Main {
           } catch (Exception e) {
             e.printStackTrace();
           }
-        }
-      }
     }
+}
+}
+
